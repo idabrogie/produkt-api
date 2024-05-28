ab4ab9d2f9887147845b7053e9e92e7ef1f9f4e9
phinds77 <par.hinds@knowit.se>
2024-05-28 10:07:28 +0200

    Update unit test with end point Hello  World

--- src/test/java/com/example/produktapi/controller/ProductControllerTest.java
+++ src/test/java/com/example/produktapi/controller/ProductControllerTest.java
@@ -1,54 +1,105 @@
 package com.example.produktapi.controller;
 
-import com.github.tomakehurst.wiremock.WireMockServer;
-import com.github.tomakehurst.wiremock.client.WireMock;
-import com.github.tomakehurst.wiremock.junit.WireMockRule;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import java.util.Locale;
+import com.example.produktapi.model.Product;
+import com.example.produktapi.service.ProductService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.io.IOException;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.*;
-import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
-import static org.junit.Assert.assertEquals;
 
+@SpringBootTest
 class ProductControllerTest {
-    WireMockServer wm;
+
+    private MockMvc mockMvc;
+
+    @InjectMocks
+    private ProductController productController;
+
+    @Mock
+    private ProductService productService;
+
     @BeforeEach
-    void setUp() {
-        wm = new WireMockRule(wireMockConfig().port(8080));
-        wm.start();
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(productController)
+                .build();
+    }
+
+
+
+    @Test
+    void hello() throws Exception {
+        mockMvc.perform(get("/my-endpoint"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello, world!"));
+
     }
 
     @Test
-    void hello() {
-        //        // create a stub
-        WireMock.stubFor(get(urlEqualTo("/my-endpoint")).willReturn(aResponse().withBody("Hello, world!")));
+    void testGetProductById_Exists() throws Exception {
+        Integer productId = 1;
+        Product mockProduct = createMockProduct(productId);
 
-        // call request in WireMock through OkHttpClient
-        OkHttpClient client = new OkHttpClient().newBuilder()
-                .build();
-        Request request = new Request.Builder()
-                .url("http://localhost:8080/my-endpoint")
-                .method("GET", null)
-                .build();
+        when(productService.getProductById(productId)).thenReturn(mockProduct);
 
-        Response response;
-        try {
-            response = client.newCall(request).execute();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        String expectedJson = getExpectedProductJson(mockProduct);
+        System.out.println("Expected JSON: " + expectedJson);
 
-        // assert the response
-        try {
-            Assertions.assertEquals("Hello, world!", response.body().string());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        mockMvc.perform(get("/products/{id}", productId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding("utf-8"))
+                .andDo(print()) // Print the request and response details
+                .andExpect(status().isOk());
+
+        Assertions.assertEquals(1, mockProduct.getId());
+    }
+
+    @Test
+    void testGetProductById_NotExists() throws Exception {
+        Integer productId = 2;
+        Product mockProduct = createMockProduct(productId);
+
+        when(productService.getProductById(productId)).thenReturn(mockProduct);
+
+        String expectedJson = getExpectedProductJson(mockProduct);
+        System.out.println("Expected JSON: " + expectedJson);
+
+        mockMvc.perform(get("/products/{id}", productId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding("utf-8"))
+                .andDo(print()) // Print the request and response details
+                .andExpect(status().isOk());
+
+        Assertions.assertNotEquals(1, mockProduct.getId());
+    }
+
+    private Product createMockProduct(Integer productId) {
+        Product product = new Product();
+        product.setId(productId);
+        product.setTitle("Test Product");
+        product.setPrice(100.0);
+        product.setCategory("Test Category");
+        product.setDescription("Test Description");
+        product.setImage("test_image.png");
+        return product;
+    }
+
+    private String getExpectedProductJson(Product product) {
+        return String.format(Locale.US,"{\"id\":%d,\"title\":\"%s\",\"price\":%.2f,\"category\":\"%s\",\"description\":\"%s\",\"image\":\"%s\"}",
+                product.getId(), product.getTitle(), product.getPrice(), product.getCategory(), product.getDescription(), product.getImage());
     }
 }
