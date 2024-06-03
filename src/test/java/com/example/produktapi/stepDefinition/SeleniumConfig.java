f4632d35fa34ba6e57cc9ac79a9f0fb3312c9b65
Fatima Grahn <fatimagrahn@hotmail.se>
2024-06-03 14:04:40 +0200

    Fix env-variable

--- src/test/java/com/example/produktapi/stepDefinition/SeleniumConfig.java
+++ src/test/java/com/example/produktapi/stepDefinition/SeleniumConfig.java
@@ -13,7 +13,9 @@ public class SeleniumConfig {
     public  WebDriver getDriver() {
         if (driver == null) {
             ChromeOptions options = new ChromeOptions();
-            options.addArguments("--headless=new");
+            if (Boolean.parseBoolean(System.getenv("RUN_HEADLESS"))) {
+                options.addArguments("--headless=new");
+            }
             driver = new ChromeDriver(options);
             driver.manage().window().maximize();
         }
