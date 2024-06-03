c603fc5d5f6bc8a82a8ce152cc99307f5aa0311d
fatimagrahn22 <112938490+fatimagrahn22@users.noreply.github.com>
2024-06-03 10:40:55 +0200

    38 make selenium work
    * add workflow for featurebranches
    
    * add args to mvn to run cucumber
    
    * fetch chromedriver 125
    
    * Clean up workflow-file
    
    ---------
    
    Co-authored-by: Ida Brogie <ida.brogie@gmail.com>
    Co-authored-by: phinds77 <par.hinds@knowit.se>

--- src/test/java/com/example/produktapi/stepDefinition/AddProductSteps.java
+++ src/test/java/com/example/produktapi/stepDefinition/AddProductSteps.java
@@ -1,15 +1,8 @@
 package com.example.produktapi.stepDefinition;
 
-import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+
 
 public class AddProductSteps {
 
