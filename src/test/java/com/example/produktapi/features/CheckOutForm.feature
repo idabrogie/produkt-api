5b81e31fcbecc29d0692294d285b3aa2f6fbe36e
Fatima Grahn <fatimagrahn@hotmail.se>
2024-06-07 11:05:05 +0200

    WIP - checkout form

---
+++ src/test/java/com/example/produktapi/features/CheckOutForm.feature
@@ -0,0 +1,12 @@
+
+#Gherkin:
+Feature: User wants to checkout shopcart
+
+  Background:
+    Given there are items in the shopCart
+    And the user is on CheckoutPage
+
+  Scenario: Form is missing data
+    When user does not fill in the form
+    And user clicks the continue to checkout-button
+    Then the user cannot continue to checkout
---
+++ src/test/java/com/example/produktapi/stepDefinition/CheckoutFormSteps.java
@@ -0,0 +1,45 @@
+package com.example.produktapi.stepDefinition;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class CheckoutFormSteps {
+//Fatima och Ida
+
+    SeleniumConfig seleniumConfig = new SeleniumConfig();
+
+    @Given("there are items in the shopCart")
+    public void thereAreItemsInTheShopCart() {
+        WebDriverWait wait = new WebDriverWait(seleniumConfig.getDriver(), Duration.ofSeconds(10));
+        WebElement addToCartButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[contains(text(), 'Add to cart')]")));
+
+        // Scroll into view
+        ((JavascriptExecutor) seleniumConfig.getDriver()).executeScript("arguments[0].scrollIntoView(true);", addToCartButton);
+
+        // Click using JavaScript executor
+        ((JavascriptExecutor) seleniumConfig.getDriver()).executeScript("arguments[0].click();", addToCartButton);
+    }
+
+    @And("the user is on CheckoutPage")
+    public void theUserIsOnCheckoutPage() {
+        seleniumConfig.getDriver().get(("https://webshop-agil-testautomatiserare.netlify.app/checkout"));
+    }
+
+    @When("user does not fill in the form")
+    public void userDoesNotFillInTheForm() {
+
+    }
+
+    @And("user clicks the continue to checkout-button")
+    public void userClicksTheContinueToCheckoutButton() {
+        seleniumConfig.getDriver().findElement(By.id(""));
+    }
+}
